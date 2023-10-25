f6e861107876c07ce1c1048ee4d0b9349e191d00
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-10-25 17:11:01 +0530

    added metaplex libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine"
 import './App.css';
 
 function App() {
+  // const umi = createUmi('http://localhost:8080').use(mplCandyMachine());
+  const rpcEndpoint = 'http//localhost:8899';
+  if (!rpcEndpoint) {
+    return <div>Error</div>
+  }
   return (
     <div className="App">
       <h1>Hellooo</h1>
