b657ee74b156503736dd4279f31298e545e6cf30
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-10-27 17:01:35 +0530

    added api secret key

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine"
-import { generateSigner } from '@metaplex-foundation/umi';
-import { TokenStandard } from "@metaplex-foundation/mpl-token-metadata"
-import './App.css';
+import React, { useEffect } from "react";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+import { generateSigner, percentAmount } from "@metaplex-foundation/umi";
+import {
+  TokenStandard,
+  createNft,
+} from "@metaplex-foundation/mpl-token-metadata";
+import "./App.css";
 
 function App() {
-  const rpcEndpoint = 'http//localhost:8899';
+  const rpcEndpoint =
+    "https://solana-devnet.g.alchemy.com/v2/" + process.env.REACT_APP_API_KEY;
   if (!rpcEndpoint) {
-    return <div>Error</div>
+    return <div>Error</div>;
   }
 
   const umi = createUmi(rpcEndpoint).use(mplCandyMachine());
-
   const collectionUpdateAuthority = generateSigner(umi);
-
+  const collectionMint = generateSigner(umi);
+  
+  const createNftCollectHandler = async () => {
+    await createNft(umi, {
+      mint: collectionMint,
+      authority: collectionUpdateAuthority,
+      name: "Consumables",
+      uri: "http://localhost:8899/", // to be fetched from the form after uploading the data.
+      isCollection: true,
+      sellerFeeBasisPoints: percentAmount(1.0, 2), // royality fee of 1.00 %
+    }).sendAndConfirm(umi);
+  };
 
   const candyMachineSettings = {
+    collectionMint: collectionMint.publicKey,
+    collectionUpdateAuthority,
     tokenStandard: TokenStandard.NonFungible,
-    symbol: 'LUCID',
+    symbol: "LUCID",
     maxEditionSupply: 10,
     isMutable: true,
-  }
+  };
 
   return (
     <div className="App">
