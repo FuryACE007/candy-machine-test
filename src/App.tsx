65fb438fa4f71e98151e70ef5d91345f02389976
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-10-27 11:53:42 +0530

    added metaplex libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine"
+import { generateSigner } from '@metaplex-foundation/umi';
+import { TokenStandard } from "@metaplex-foundation/mpl-token-metadata"
 import './App.css';
 
 function App() {
-  // const umi = createUmi('http://localhost:8080').use(mplCandyMachine());
   const rpcEndpoint = 'http//localhost:8899';
   if (!rpcEndpoint) {
     return <div>Error</div>
   }
+
+  const umi = createUmi(rpcEndpoint).use(mplCandyMachine());
+
+  const candyMachineSettings = {
+    tokenStandard: TokenStandard.NonFungible,
+    symbol: 'LUCID',
+    maxEditionSupply: 10,
+    isMutable: true,
+  }
+
   return (
     <div className="App">
       <h1>Hellooo</h1>
